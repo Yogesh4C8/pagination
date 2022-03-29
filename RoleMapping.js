fa99464d612d1c7f8b7c1d228ef7739b0638dc32
Yogesh kumar <84324173+Yogesh4C8@users.noreply.github.com>
2022-03-29 17:48:30 +0530

    Create RoleMapping.js

---
+++ RoleMapping.js
@@ -0,0 +1,23 @@
+ <div style={{height:"300px",width:"300px",display:"flex",justifyContent:"center",alignItems:"center"}}>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>User Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter Name" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter First Name" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter Last Name" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Enter Email" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+            </Form>
+      </div>
